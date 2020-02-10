73f731356cea1bdfc626d1052e89473294591906
m <mactechy@gmail.com>
2020-02-10 13:49:47 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>TechnoColor: Melissa's portfolio website</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/styles.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
+
+</head>
+
+<body>
+
+	<header>
+		<div id="top" class="logo"><a href="/"><img src="assets/img/logo.png" alt="logo of site"></a></div>
+		<nav>
+			<ul>
+			<li><a href="#Home">Home</a></li>
+			<li><a href="#portfolio">Portfolio</a></li>
+			<li><a href="#about">Skills</a></li>
+			<li><a href="#contact">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(assets/img/hero.jpg);"></div>
+		<h1>Hello & Welcome!</h1>
+		<h3>This is a portfolio website of a developer & designer</h3>
+	</section>
+
+
+	<section id="portfolio" class="our-work">
+		<h3 class="title">Portfolio</h3>
+		<p>Collection of Examples of UX, UI, Branding, Web Development, and Graphic Design Work</p>
+		<hr>
+<p class="msg"> *Please click on the images below to see more designs.*</p>
+		<ul class="port-grid">
+			<li class="small"><a href="assets/img/port/website.png" data-fancybox="websites" data-caption="Business Website "><img src="assets/img/port/website.png" alt="business website" /><div class="text-wrapper">Website Design/Development</div></a></li>
+
+			<li class="hide"><a href="assets/img/port/bamboo.png" data-fancybox="websites" data-caption="A nonprofit website"><img src="assets/img/port/bamboo.png" alt="website nonprofit sample" /><div class="text-wrapper">Website Design/Development</div></a></li>
+
+
+			<li class="small"><a href="assets/img/port/ui.png" data-fancybox="UX"  data-caption="Form UI Design"><img src="assets/img/port/ui.png" alt="Form UI" /><div class="text-wrapper">UI/UX</div></a></li>
+
+			<li class="hide"><a href="assets/img/port/icons.png" data-fancybox="UX" data-caption="Designed Icons"><img src="assets/img/port/icons.png" alt="Icon Design" /><div class="text-wrapper">Icon Design</div></a></li>
+
+
+			<li class="small"><a href="assets/img/port/cats-charms.png" data-fancybox="products" data-caption="Acrylic Charm Design"><img src="assets/img/port/cats-charms.png" alt="charm design" /><div class="text-wrapper">Print/Product Design</div></a></li>
+
+			<li class="hide"><a href="assets/img/port/drink-coaster.png" data-fancybox="products" data-caption="Drink Coaster Design"><img src="assets/img/port/drink-coaster.png" alt="Drink Coaster Design" /><div class="text-wrapper">Print/Product Design</div></a></li>
+
+			<li class="hide"><a href="assets/img/port/sticker-transfer.png" data-fancybox="products" data-caption="Sticker Design"><img src="assets/img/port/sticker-transfer.png" alt="Sticker Design" /><span class="text-wrapper">Print Design</span></a></li>
+
+
+			<li class="small"><a href="assets/img/port/mobile.jpg" data-fancybox data-caption="Mobile Application Design"><img src="assets/img/port/mobile.jpg" alt="mobile site design" /><div class="text-wrapper">Mobile Application Design</div></a></li>
+
+
+			<li class="small"><a href="assets/img/port/colorful-spiral.png" data-fancybox="Graphic Design" data-caption="Design"><img src="assets/img/port/colorful-spiral.png" alt="charm design" /><div class="text-wrapper">Graphic Design</div></a></li>
+
+
+
+			<li class="small"><a href="assets/img/port/logo.png" data-fancybox data-caption="Logo Design"><img src="assets/img/port/logo.png" alt="logo design" /><div class="text-wrapper">Logo Design</div></a></li>
+
+
+
+
+
+
+	</section>
+
+
+	<section id="about" class="features">
+		<h3 class="title">Skills</h3>
+		<p>As a techy designer-developer, I am very passionate and knowledgeable in building a website from the ground up, doing server admin or creating design collateral</p>
+		<hr>
+
+		<ul class="grid">
+			<li>
+				<i class="fa fa-camera-retro"></i>
+				<h4>Designer</h4>
+				<p> A graphic/web designer with a passion for designing and creating beautiful and functional pieces. </p>
+
+				<p>&nbsp;</p>
+
+
+				<p>
+					<span class="heading-skills">Business Collateral:</span> business cards, brochures, logos, branding, etc. </p>
+					<hr>
+					<p> <span class="heading-skills">Marketing:</span> social media, e-blasts, newsletters, etc.</p>
+					<hr>
+					<p> <span class="heading-skills">Web/Digital:</span> website/mobile layouts, design systems,  UI elements, etc.   </p>
+					<hr>
+					<p> <span class="heading-skills">Misc:</span> t-shirts, stickers, drink coasters, buttons, banners, labels, etc. </p>
+			</li>
+			<li>
+				<i class="fa fa-cubes"></i>
+				<h4>Web Development</h4>
+				<p> I build responsive mobile friendly websites from scratch and write clean semantic code.</p>
+				<p>&nbsp;</p>
+
+				<p> <span class="heading-skills">Platforms:</span> WordPress, Drupal, Expression Engine, BigCommerce, Magento, WooCommerce, Shopify and Hubspot </p>
+				<hr>
+				<p> <span class="heading-skills">Technologies:</span> HTML, CSS, Sass, MySQL, PHP, JavaScript, and jQuery </p>
+			</li>
+
+			<li>
+				<i class="fa fa-server"></i>
+				<h4>IT </h4>
+				<p> A geek at heart, I am passionate about technology. </p>
+				<p>&nbsp;</p>
+				&nbsp;
+				&nbsp;
+				<p> <span class="heading-skills">Skills:</span> Mac/PC tech support, web hosting support, and server management and support</p>
+
+				<hr>
+				&nbsp;
+				<p> <span class="heading-skills">Tech</span> Apache, IIS, Nginx, Docker, Vagrant, DNS, Bash,MySQL, MariaDB, cPanel, sFTP, and SSH</p>
+
+				</li>
+				</ul>
+		</div>
+	</section>
+
+
+	<!--<section class="reviews">
+		<h3 class="title">What others say:</h3>
+
+		<p class="quote">What to say.</p>
+		<p class="author">— Awesome</p>
+
+
+	</section>
+-->
+
+	<section id="contact" class="contact">
+		<h3 class="title">Contact Me</h3>
+		<p>Please contact me below for full work history, portfolio, or about needing help with your project.</p>
+
+		 <form id="contact-form" method="post" action="" role="form">
+
+                    <div class="messages"></div>
+
+                    <div class="controls">
+                        <div class="row">
+                            <div class="column">
+								<div class="form-group">
+                                    <label for="form_name">First Name *</label>
+                                    <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required"
+                                        data-error="Firstname is required.">
+                                    <div class="help-block with-errors"></div>
+
+								</div>
+							</div>
+
+							<div class="column">
+                                <div class="form-group">
+                                    <label for="form_lastname">Last Name *</label>
+                                    <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your lastname *" required="required"
+                                        data-error="Lastname is required.">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+						</div>
+
+
+
+
+                        <div class="row">
+                            <div class="column">
+                                <div class="form-group">
+                                    <label for="form_email">Email *</label>
+                                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required"
+                                        data-error="Valid email is required.">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div class="column">
+                                <div class="form-group">
+                                    <label for="form_phone">Phone</label>
+                                    <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+						</div>
+
+						<div class="row">
+							<div class="column">
+                        <div class="form-group">
+                            <label for="form_message">Message *</label>
+                            <textarea id="form_message" name="message" class="form-control" placeholder="Say Hello *" rows="10" cols="80" required="required"
+                                data-error="Please, leave me a message."></textarea>
+                            <div class="help-block with-errors"></div>
+                        </div>
+					</div>
+				</div>
+				<div class="row">
+						<div class="column">
+                        <div class="form-group">
+                            <div class="g-recaptcha" data-sitekey="" data-callback="verifyRecaptchaCallback" data-expired-callback="expiredRecaptchaCallback"></div>
+                            <div class="help-block with-errors"></div>
+                        </div>
+
+
+                        <input type="submit" class="btn btn-success btn-send" value="Send message">
+					</div>
+				</div>
+                        <p class="text-muted">
+                            <strong>*</strong> These fields are required.
+                        </p>
+
+                    </div>
+
+                </form>
+
+		<hr>
+
+
+	</section>
+
+	<footer>
+
+		<p> SOCIAL MEDIA LINKS</p>
+		<ul>
+			<li><a href="http://dribbble.com/spxds" target="_blank"><i class="fa fa-dribbble"></i></a></li>
+			<li><a href="https://github.com/spxds" target="_blank"><i class="fa fa-github"></i></a></li>
+			<li><a href="https://twitter.com/spxds" target="_blank"><i class="fa fa-twitter"></i></a></li>
+			<li><a href="https://open.spotify.com/user/spxds?si=6U_1_-odRqefmm0QELSy0w" target="_blank"><i class="fa fa-spotify"></i></a></li>
+			<li><a href="https://www.last.fm/user/spxds" target="_blank"><i class="fa fa-lastfm"></i></a></li>
+			<li><a href="https://www.purplestars.com"><i class="fa fa-star"></i></a></li>
+
+		</ul>
+		<p><a href="#top"><i class="fa fa-arrow-circle-up"></i></a></p>
+
+		<p>©2000-<?php echo date("Y"); ?>, all rights reserved. TechnoColor</p>
+	</footer>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+
+<script type="text/javascript" src="assets/js/custom.js"></script>
+
+<!--  Google recaptcha api here -->
+
+<!--  Google Analytics  here -->
+
+
+</body>
+
+</html>
